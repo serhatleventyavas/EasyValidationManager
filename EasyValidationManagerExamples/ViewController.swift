0beca642899b89be046e163213df1cf291b22f35
serhatleventyavas <serhatlevent910@hotmail.com>
2020-02-29 18:07:15 +0300

    Release 1.0.0

--- EasyValidationManagerExamples/ViewController.swift
+++ EasyValidationManagerExamples/ViewController.swift
@@ -1,20 +1,10 @@
-//
-//  ViewController.swift
-//  EasyValidationManagerExamples
-//
-//  Created by mac on 29.02.2020.
-//  Copyright © 2020 Serhat Levent Yavaş. All rights reserved.
-//
-
 import UIKit
+import EasyValidationManager
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
