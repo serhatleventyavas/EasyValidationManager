6e26e5927252c6193734c55b1acf58a94c6ec639
serhatleventyavas <serhatlevent910@hotmail.com>
2020-02-29 17:35:08 +0300

    Initial project setup

---
+++ EasyValidationManager/EasyValidationManager.h
@@ -0,0 +1,19 @@
+//
+//  EasyValidationManager.h
+//  EasyValidationManager
+//
+//  Created by mac on 29.02.2020.
+//  Copyright © 2020 Serhat Levent Yavaş. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasyValidationManager.
+FOUNDATION_EXPORT double EasyValidationManagerVersionNumber;
+
+//! Project version string for EasyValidationManager.
+FOUNDATION_EXPORT const unsigned char EasyValidationManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyValidationManager/PublicHeader.h>
+
+
